d03256f3c92a87d8e99bc6e7e71e9f9da1c6805f
Niels de Hoog <njdehoog@gmail.com>
2022-05-19 22:39:04 +0100

    Update index.js
    Disable 'package.json' export warnings

--- index.js
+++ index.js
@@ -136,8 +136,8 @@ module.exports = function (options = {}) {
 		 */
 		generateBundle() {
 			if (pkg_export_errors.size > 0) {
-				console.warn(`\n${PREFIX} The following packages did not export their \`package.json\` file so we could not check the "svelte" field. If you had difficulties importing svelte components from a package, then please contact the author and ask them to export the package.json file.\n`);
-				console.warn(Array.from(pkg_export_errors, s => `- ${s}`).join('\n') + '\n');
+				// console.warn(`\n${PREFIX} The following packages did not export their \`package.json\` file so we could not check the "svelte" field. If you had difficulties importing svelte components from a package, then please contact the author and ask them to export the package.json file.\n`);
+				// console.warn(Array.from(pkg_export_errors, s => `- ${s}`).join('\n') + '\n');
 			}
 		}
 	};
